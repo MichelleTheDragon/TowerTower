2c08f72a07162506668dfc76c333ede8f06deff7
MichelleTheDragon <60585217+MichelleTheDragon@users.noreply.github.com>
2021-11-11 12:14:50 +0100

    Add files via upload

---
+++ Projectile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+
+namespace Tower_Defense
+{
+    public abstract class Projectile
+    {
+        public int speed = 10;
+
+        public Projectile(Texture2D tex, Point position) : base(tex, position) { }
+
+        public abstract bool Move();
+
+        public abstract void Damage();
+
+        public abstract void OnHit(Enemy other);
+
+        public abstract void LoadContent(ContentManager content);
+
+        public abstract void Update(GameTime gameTime);
+
+        public abstract void Draw(SpriteBatch spriteBatch);
+    }
+
+}
